03cfe62b2a53bf0232e96a1b03b0b104bd24e54f
Matthew Jensen <mjensen@cokergroup.com>
2021-06-12 23:03:49 -0700

    Updates to code

--- app.py
+++ app.py
@@ -12,22 +12,18 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
-
-    # Find one record of data from the mongo database
+def index():
     mars_dict = mongo.db.mars_dict.find_one()
-    # Return template and data
     return render_template("index.html", mars=mars_dict)
 
 
 @app.route("/scrape")
-def scrape():
-  
+def scraper():
     mars_dict = mongo.db.mars_dict
     mars_data = scrape_mars.scrape()
     # Update the Mongo database using update and upsert=True
     mars_dict.update({}, mars_data, upsert=True)
-    return redirect("/")
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -17,21 +17,15 @@ def scrape():
     mars_dict={}
 
     # ### NASA Mars News (https://redplanetscience.com/)
-
     # URL for NASA news page to be scraped
     news_url = 'https://redplanetscience.com/'
     browser.visit( news_url)
-
     # Create HTML object
     html = browser.html
-
     # Parse HTML with BeautifulSoup
     soup = bs( html, 'html.parser')
-
-
     # Retrieve the latest news title
     title = soup.find_all( 'div', class_ = 'content_title')[0].text
-
     # Retrieve the latest news paragraph
     paragraph = soup.find_all( 'div', class_ = 'article_teaser_body')[0].text
     print( title)
@@ -40,17 +34,13 @@ def scrape():
 
 
     # ### JPL Mars Space Images - Featured Image (https://spaceimages-mars.com/)
-
     # URL for JPL Featured Image
     space_images_url = 'https://spaceimages-mars.com/'
     browser.visit( space_images_url)
-
     # Create HTML object
     html = browser.html
-
     # Parse HTML with BeautifulSoup
     soup = bs( html, 'html.parser')
-
     # Find featured image and print source url
     featured_image = soup.find_all( 'img', class_ = 'headerimage fade-in')[0]['src']
     featured_image_url = space_images_url + featured_image
@@ -58,68 +48,52 @@ def scrape():
 
 
     # ### Mars Facts (https://galaxyfacts-mars.com/)
-
     #URL for Mars Facts Website
     facts_url = 'https://galaxyfacts-mars.com/'
-
     # Use pandas to read table of facts comparing Mars and Earth
     facts_table = pd.read_html(facts_url, header=0)
     facts_table[0]
-
     # Use pandas to read table of Mars facts
     facts_table = pd.read_html(facts_url)
     facts_df = facts_table[1]
     facts_df.columns =['Description', 'Value']
     facts_df
-
     # Convert to HTML
     facts_html = facts_df.to_html()
     print(facts_html)
 
 
     # ### Mars Hemispheres (https://marshemispheres.com/)
-
-    # URL for JPL Featured Image
+    # URL for Mars Hemispheres Images
     hemispheres_url = 'https://marshemispheres.com/'
     browser.visit( hemispheres_url)
-
     # Create HTML object
     html = browser.html
-
     # Parse HTML with BeautifulSoup
     soup = bs( html, 'html.parser')
-
     items = soup.find_all('div', class_='item')
-
     mars_hemi_urls = []
     mars_hemi_titles = []
-
     for item in items:
         mars_hemi_urls.append( hemispheres_url + item.find('a')['href'])
         mars_hemi_titles.append( item.find('h3').text.strip())
-
     print( mars_hemi_urls)
     mars_hemi_titles
-
     hemi_img_urls = []
-
     for url in mars_hemi_urls:
         browser.visit(url)
         html = browser.html
         soup = bs(html, 'html.parser')
-        
-        # Find image urls and append to list
+            # Find image urls and append to list
         source_url = hemispheres_url + soup.find('img',class_='wide-image')['src']
         hemi_img_urls.append( source_url)
-        
     hemi_img_urls
 
     hemi_img_list = []
-
     for i in range( len( mars_hemi_titles)):
         hemi_img_list.append({ 'title':mars_hemi_titles[i], 'img_url':hemi_img_urls[i]})
-
     hemi_img_list
+
 
     mars_dict = {
         "news_title":title,
